db55281f19b0de18e6dc0bc9354c87f73b4df823
hyeonsangjeon <wingnut0310@gmail.com>
2019-05-24 16:02:17 +0900

    creation user and grant and schema

--- sqldir/create_user.sql
+++ sqldir/create_user.sql
@@ -2,7 +2,14 @@ create user 'testuser'@'%' identified by 'iamgroot';
 create user 'testuser'@'localhost' identified by 'iamgroot';
 create user 'root'@'localhost' identified by 'iamgroot';
 
+
 grant all privileges on *.* to 'testuser'@'%';
 grant all privileges on *.* to 'testuser'@'localhost';
 grant all privileges on *.* to 'root'@'localhost';
+
+
+CREATE SCHEMA IF NOT EXISTS `MODEL` DEFAULT CHARACTER SET utf8 ;
+USE `MODEL` ;
+
+FLUSH PRIVILEGES;
 
